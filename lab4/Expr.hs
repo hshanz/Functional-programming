097a7f1ba147bdc69b411668be379f8387f08cd6
Puzzel123 <jakobhenriksson@live.se>
2021-12-29 13:36:39 +0100

    Hello

--- lab4/Expr.hs
+++ lab4/Expr.hs
@@ -36,19 +36,25 @@ size (Cos expr) = size(expr) + 1
 size (Sin expr) = size(expr) + 1
 size expr = size expr 
 
-showMul :: Expr -> String
-showMul (Add exp1 exp2) = "(" ++ showExpr (Add exp1 exp2) ++ ")"
+showMul :: Expr -> String 
+showMul (Add exp1 exp2) = "(" ++ showExpr exp1 ++ " + " ++ showExpr exp2 ++ ")"
 showMul exp = showExpr exp
 
 showFunc :: Expr -> String
-showFunc (Add exp1 exp2) = "(" ++ showExpr exp1 ++ " + " ++ showExpr exp2 ++ ")"
-showFunc (Mul exp1 exp2) = "(" ++ showExpr exp1 ++ "*" ++ showExpr exp2 ++ ")"
+showFunc (Add exp1 exp2) = "(" ++ "(" ++ showExpr exp1 ++ " + " ++ showExpr exp2 ++ ")" ++ ")"
+showFunc (Mul exp1 exp2) = "(" ++ "(" ++ showExpr exp1 ++ ")" ++ "*" ++ "(" ++ showExpr exp2 ++ ")" ++ ")"
+showFunc (Sin exp)      = "(" ++ "sin " ++ "(" ++ showExpr exp ++ ")" ++ ")"
+showFunc (Cos exp)      = "(" ++ "cos" ++ "(" ++ showExpr exp ++ ")" ++ ")"
 showFunc exp = showExpr exp
+
 
 showExpr :: Expr -> String
 showExpr (Num n) = show n
+showExpr (Mul (Add exp1 exp2) exp)  = showMul (Add exp1 exp2) ++ "*" ++showMul exp 
+showExpr (Mul exp (Add exp1 exp2))  = showMul exp ++ "*" ++ showMul (Add exp1 exp2)
 showExpr (Add exp1 exp2) = showExpr exp1 ++ " + " ++ showExpr exp2
 showExpr (Mul exp1 exp2) = showMul exp1 ++ "*" ++ showExpr exp2
+
 showExpr (Sin exp)       = "sin " ++ showFunc exp
 showExpr (Cos exp)       = "cos " ++ showFunc exp 
 showExpr (X)             = "x"
@@ -93,13 +99,32 @@ readStr :: String -> Maybe Expr
 readStr s = Just (fst (fromJust (parse expr (filter (not.isSpace) s))))
         
 prop_ShowReadExpr :: Expr -> Bool
-prop_ShowReadExpr exp = exp == fromJust (readStr (showExpr exp))
+prop_ShowReadExpr exp = simplify exp  == simplify (fromJust (readStr (showExpr exp))) 
 
 
-genNum   :: Gen Expr
-genNum = do
-         x <- arbitrary
-         return (Num x)
+
+arbOp :: Gen Expr -> Gen Expr  -> Gen Expr
+arbOp e1 e2  = do e1' <- e1
+                  e2' <- e2
+                  elements[Add e1' e2', Mul e1' e2']
+arbExpr :: Int -> Gen Expr
+arbExpr i = frequency [(1,genNum),(i,arbOp (arbExpr $ i `div` 2) (arbExpr $ i `div` 2)), (i, arbFun (arbExpr $ i `div` 2))]
+
+
+arbFun :: Gen Expr -> Gen Expr
+arbFun e = do
+            e' <- e
+            elements[Cos e', Sin e']
+
+genNum :: Gen Expr
+genNum =  do
+          Num <$> arbitrary
+
+
+
+instance Arbitrary Expr where
+    arbitrary = sized arbExpr
+
 
 simplify :: Expr -> Expr
 simplify (Num n) = Num n
@@ -107,10 +132,10 @@ simplify X = X
 
 simplify (Add (Num 0) expr)         = simplify expr
 simplify (Add expr (Num 0))         = simplify expr
-simplify (Add (Num n1) (Num n2))    = Num (n1 + n2)
-simplify (Add expr X)               = (Add (simplify expr) X)
-simplify (Add X expr)               = (Add (simplify expr) X)
-simplify (Add expr1 expr2)          = (Add (simplify expr1) (simplify expr2))
+simplify (Add (Num n1) (Num n2))    = Num (eval (Add (Num n1) (Num n2)) 1)
+simplify (Add expr X)               = Add (simplify expr) X
+simplify (Add X expr)               = Add (simplify expr) X
+simplify (Add expr1 expr2)          = simplify (Add (simplify expr1) (simplify expr2))
 
 simplify (Mul expr (Num 0))         = Num 0
 simplify (Mul (Num 0) expr)         = Num 0
@@ -118,13 +143,14 @@ simplify (Mul (Num 1) expr)         = simplify expr
 simplify (Mul expr (Num 1))         = simplify expr
 simplify (Mul expr X)               = Mul (simplify expr) X
 simplify (Mul X expr)               = Mul (simplify expr) X
-simplify (Mul (Num n1) (Num n2))    = Num (n1 * n2)
+simplify (Mul (Num n1) (Num n2))    = Num (eval (Mul (Num n1) (Num n2)) 1)
 simplify (Mul expr1 expr2)          = simplify (Mul (simplify expr1) (simplify expr2))
 
-simplify (Sin (Num n))              = Num (Prelude.sin n) 
-simplify (Cos (Num n))              = Num (Prelude.cos n)
-simplify (Sin (expr))               = simplify (Sin (simplify expr))
-simplify (Cos (expr))               = simplify (Cos (simplify expr))
+
+simplify (Sin (Num n))              = Num (eval (Sin (Num n)) 1)
+simplify (Cos (Num n))              = Num (eval (Cos (Num n)) 1)
+simplify (Sin (expr))               = Sin (simplify expr)
+simplify (Cos (expr))               = Cos (simplify expr)
 
 differentiate :: Expr -> Expr
 differentiate expr = simplify (differentiate' (simplify expr))
@@ -132,10 +158,10 @@ differentiate expr = simplify (differentiate' (simplify expr))
 differentiate' :: Expr -> Expr
 
 --Addition Rule
-differentiate' (Add expr1 expr2)           = (Add (differentiate' expr1) (differentiate' expr1))
+differentiate' (Add expr1 expr2)   = (Add (differentiate' expr1) (differentiate' expr2))
 
 --Product Rule
-differentiate' (Mul expr1 expr2)           = (Add (Mul expr1 (differentiate' expr2)) (Mul expr2 (differentiate' expr1)))
+differentiate' (Mul expr1 expr2)   = (Add (Mul expr1 (differentiate' expr2)) (Mul expr2 (differentiate' expr1)))
 
 --Sin rule
 differentiate' (Sin expr)          = (Mul (differentiate' expr) (Cos (expr)))
